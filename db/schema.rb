53d81a9807bf582e6edfcbca3b269f9a30c6a0d5
Joshua Retallick <josh@old-empire.co.uk>
2021-04-16 16:16:58 +0100

    Added category options to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_143418) do
+ActiveRecord::Schema.define(version: 2021_04_16_150955) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
