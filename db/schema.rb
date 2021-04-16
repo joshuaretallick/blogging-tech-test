483e4fe04f82eb0307726a82d6b0a5bf8173aa3c
Joshua Retallick <josh@old-empire.co.uk>
2021-04-16 18:30:21 +0100

    Added user association so they own an article and show in view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_152210) do
+ActiveRecord::Schema.define(version: 2021_04_16_171800) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_152210) do
     t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
